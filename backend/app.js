f77039366a825b5c1ccbe7563b067c4d63d64bb7
sundaram123krishnan <krishnsundaram@gmail.com>
2023-04-16 12:26:02 +0530

    added put and delete method along with cors

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./routes/route");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- backend/routes/route.js
+++ backend/routes/route.js
@@ -13,6 +13,7 @@ router.get("/", async (req, res) => {
     res.status(404).send(`Oops! An error occurred, ${error}`);
   }
 });
+
 router.post("/", async (req, res) => {
   try {
     const blog = await product.create(req.body);
@@ -21,5 +22,45 @@ router.post("/", async (req, res) => {
     res.status(404).send(`Oops! An error occurred, ${error}`);
   }
 });
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const blog = await product.findById(id);
+    if (!blog) {
+      return res.status(200).json({ message: "Cannot find the blog" });
+    }
+    return res.status(200).json(blog);
+  } catch (error) {
+    res.status(404).send(`Oops! An error occurred, ${error}`);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const blog = await product.findByIdAndUpdate(id, req.body);
+    if (!blog) {
+      return res.status(200).json({ message: "Cannot update the blog" });
+    }
+    return res.status(200).json(blog);
+  } catch (error) {
+    res.status(404).send(`Oops! An error occurred, ${error}`);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const blog = await product.findByIdAndDelete(id);
+    if (!blog) {
+      return res.status(200).json({ message: "Cannot delete the user" });
+    }
+    return res.status(200).json(blog);
+  } catch (error) {
+    res.status(404).send(`Oops! An error occurred, ${error}`);
+  }
+});
+
 
 module.exports = router;
