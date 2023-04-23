8847ff5006e122bd95ceb180742f63a1f5d4f953
sundaram123krishnan <krishnsundaram@gmail.com>
2023-04-23 18:43:12 +0530

    added home page

---
+++ frontend/src/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center gap-10 bg-gradient-to-r from-purple-200 via-purple-400 to-purple-800 h-screen">
+      <h1 className="text-7xl font-bold text-center mt-32">
+        Welcome to our Blog website
+      </h1>
+    </div>
+  );
+}
