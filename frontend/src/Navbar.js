dbd7a064fe3363c3961ceb94fabc193743887e16
sundaram123krishnan <krishnsundaram@gmail.com>
2023-04-23 18:43:25 +0530

    added home section to navbar

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
-import Add from "./Add";
+import ControlledEditor from "./Add";
 import Content from "./Content";
+import Home from "./Home";
 
 export default function Navbar() {
   return (
     <BrowserRouter>
       <div>
         <div className="flex justify-evenly p-5 bg-black text-white">
+          <button>
+            <NavLink to="/">HOME</NavLink>
+          </button>
           <button>
             <NavLink to="/add">ADD</NavLink>
           </button>
@@ -17,8 +21,9 @@ export default function Navbar() {
         </div>
       </div>
       <Routes>
-        <Route exact path="/add" element={<Add />} />
+        <Route exact path="/add" element={<ControlledEditor />} />
         <Route exact path="/blogs" element={<Content />} />
+        <Route exact path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
