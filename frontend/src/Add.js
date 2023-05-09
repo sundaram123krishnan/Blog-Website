22ed8cb39fc360c445b6bce9ed8143f13677bfa0
sundaram123krishnan <krishnsundaram@gmail.com>
2023-05-09 15:11:06 +0530

    resolved bugs

--- frontend/src/Add.js
+++ frontend/src/Add.js
@@ -10,7 +10,7 @@ import SubScript from "@tiptap/extension-subscript";
 const content =
   '<h2 style="text-align: center;">Welcome to Mantine rich text editor</h2><p><code>RichTextEditor</code> component focuses on usability and is designed to be as simple as possible to bring a familiar editing experience to regular users. <code>RichTextEditor</code> is based on <a href="https://tiptap.dev/" rel="noopener noreferrer" target="_blank">Tiptap.dev</a> and supports all of its features:</p><ul><li>General text formatting: <strong>bold</strong>, <em>italic</em>, <u>underline</u>, <s>strike-through</s> </li><li>Headings (h1-h6)</li><li>Sub and super scripts (<sup>&lt;sup /&gt;</sup> and <sub>&lt;sub /&gt;</sub> tags)</li><li>Ordered and bullet lists</li><li>Text align&nbsp;</li><li>And all <a href="https://tiptap.dev/extensions" target="_blank" rel="noopener noreferrer">other extensions</a></li></ul>';
 
-export default function Demo() {
+function Demo() {
   const editor = useEditor({
     extensions: [
       StarterKit,
@@ -23,8 +23,6 @@ export default function Demo() {
     ],
     content,
   });
-
-  console.log(editor);
 
   return (
     <RichTextEditor editor={editor}>
@@ -46,27 +44,6 @@ export default function Demo() {
           <RichTextEditor.H4 />
         </RichTextEditor.ControlsGroup>
 
-<<<<<<< HEAD
-    
-    return (
-      <div className="flex flex-col items-center justify-center mt-10 gap-10">
-        <div className="text-3xl font-bold">
-          <h1>Add your blog here</h1>
-        </div>
-        <form action="" className="flex flex-col items-center">
-          <Editor
-            editorState={editorState}
-            wrapperClassName="demo-wrapper"
-            editorClassName="demo-editor"
-            onEditorStateChange={this.onEditorStateChange}
-            onContentStateChange={this.onContentStateChange}
-          />
-          <button className="border-2 w-max px-5 py-2 ">ADD</button>
-        </form>
-      </div>
-    );
-  }
-=======
         <RichTextEditor.ControlsGroup>
           <RichTextEditor.Blockquote />
           <RichTextEditor.Hr />
@@ -92,5 +69,4 @@ export default function Demo() {
       <RichTextEditor.Content />
     </RichTextEditor>
   );
->>>>>>> a1cbe617a8b57498fa690a8952eee04793981900
 }
